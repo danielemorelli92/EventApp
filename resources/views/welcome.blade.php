0aae11872819495a2f1c62175ae22b1baa662a0d
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-05-11 16:14:08 +0200

    Tristemente, togliamo il controllo email perché ci da problemi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,6 +124,8 @@
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+
+                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAHH</h1>
                     </div>
                 </div>
             </div>
