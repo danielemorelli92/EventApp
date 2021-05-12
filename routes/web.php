ca908eed43a6d7ef74c56fa2c3c1fb068d6a34ba
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-05-12 19:02:15 +0200

    EA-37 Scheletro della home page, lista eventi e pagina evento.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/events', function () {
+    return view('events');
+});
+Route::get('/event', function () {
+    return view('event');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
