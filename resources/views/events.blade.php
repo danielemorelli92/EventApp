66963f9cbc28af7169ca9241d773d4553a0c3e1d
solazzol <solazzolorenzo@libero.it>
2021-05-16 17:29:36 +0200

    Lavori in corso

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,12 +1,46 @@
 @extends('layouts.layout')
 
+@section ('style')
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/events.css') }}"
+@endsection
+
 @section('content')
-    <h1>Lista degli eventi</h1>
-    <ul>
-        <li><a href="/event">Evento 1</a></li>
-        <li><a href="/event">Evento 2</a></li>
-        etc...
-    </ul>
+
+    <header>
+        <nav>
+            <h1>EventApp</h1>
+            <div data-role="header" data-position="inline">
+                <div class="btn-group">
+                    <form method="get" action="URL_PAGINA_WEB">
+                       <a><button>Home</button></a>
+                    </form>
+                    <form method="get" action="URL_PAGINA_WEB">
+                        <a><button>Inserire immagini</button></a>
+                    </form>
+                    <form method="get" action="URL_PAGINA_WEB">
+                        <button>Creare bottoni con i CSS</button>
+                    </form>
+                    <form method="get" action="URL_PAGINA_WEB">
+                        <button>Creare forms con i CSS</button>
+                    </form>
+                    <form method="get" action="URL_PAGINA_WEB">
+                        <button style="width:20px">Creare tabelle con i CSS</button></form>
+                    <form method="get" action="URL_PAGINA_WEB">
+                        <button style="width:20px">Altri elementi con i CSS</button></form>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <h1>Eventi in base ai filtri</h1>
+
+    <div class="container">
+        <div class="toppane">Test Page</div>
+        <div class="leftpane">
+            <h1>Test Page</h1></div>
+        <div class="rightpane">
+            <h1>Test Page</h1></div>
+    </div>
 
     <a href="/">Back</a>
 @endsection
