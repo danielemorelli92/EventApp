3e7b30da107abf10e266953102c4b574ca4d5c99
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-05-16 22:59:33 +0200

    Aggiornamento dei test

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,14 +16,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'firstname',
-        'email',
-        'password',
-        'username',
-        'lastname',
-        'birthday'
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
