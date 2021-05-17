fd1681cb2fc58250e0e5b5c46bc2a30ad9ec4103
Piereligio <piereligio@gmail.com>
2021-05-17 19:05:30 +0200

    Fix pagina lista eventi HTML

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -8,25 +8,38 @@
 
 
     <div class="main-content-column">
-        <h2>Eventi in base ai filtri</h2>
+        <div class="section-title">Eventi in base ai filtri</div>
         <div class="events-list">
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
-            <div class="event-item-rectangle"></div>
+            <div class="event-rectangle">
+                <div class="event-rectangle-image-container">
+                    <!--TODO inserire immagine <img src="imgBOH" alt="BOHBOH">-->
+                </div>
+                <div class="event-rectangle-title">Event title</div>
+                <div class="event-rectangle-attributes-group">
+                    <div class="event-rectangle-attribute">
+                        Category
+                    </div>
+                    <div class="event-rectangle-attribute">
+                        Place
+                    </div>
+                    <div class="event-rectangle-attribute">
+                        Date
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
+
     <div class="right-side-column">
-        <h1>Right</h1></div>
+        <div class="section-title">Filtro distanza/data</div>
+        <div class="events-filters-selection-box">
+
+        </div>
+        <div class="section-title">Interessi</div>
+        <div class="interests-selection-box">
+
+        </div>
+    </div>
 
 
     <!--
