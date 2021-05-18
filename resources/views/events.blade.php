1e9abf7e9d5b43be02b33ac6e445ac14b92c4fb6
Piereligio <piereligio@gmail.com>
2021-05-18 12:11:24 +0200

    Alcuni fix

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -11,23 +11,23 @@
         <div class="main-content-column">
             <div class="section-title">Eventi in base ai filtri</div>
             <div class="events-list">
-                <div class="event-rectangle">
-                    <div class="event-rectangle-image-container">
-                        <!--TODO inserire immagine <img src="imgBOH" alt="BOHBOH">-->
-                    </div>
-                    <div class="event-rectangle-title">Event title</div>
-                    <div class="event-rectangle-attributes-group">
-                        <div class="event-rectangle-attribute">
-                            Category
+                <a class="event-rectangle" href="#">
+                        <div class="event-rectangle-image-container">
+                            <!--TODO inserire immagine <img src="imgBOH" alt="BOHBOH">-->
                         </div>
-                        <div class="event-rectangle-attribute">
-                            Place
+                        <div class="event-rectangle-title">Event title</div>
+                        <div class="event-rectangle-attributes-group">
+                            <div class="event-rectangle-attribute">
+                                Category
+                            </div>
+                            <div class="event-rectangle-attribute">
+                                Place
+                            </div>
+                            <div class="event-rectangle-attribute">
+                                Date
+                            </div>
                         </div>
-                        <div class="event-rectangle-attribute">
-                            Date
-                        </div>
-                    </div>
-                </div>
+                </a>
             </div>
 
     </div>
