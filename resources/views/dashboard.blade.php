fc5a7b08a9f9bf1c4bd231feca57b768ce302585
Piereligio <piereligio@gmail.com>
2021-05-18 19:02:08 +0200

    Base HTML Area Personale

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,49 @@
-<x-app-layout>
+@extends('layouts.layout-header-two-columns')
+
+@section ('style')
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/events.css') }}"
+@endsection
+
+@section('content')
+    <div class="main-content-column">
+        <div class="section-title">Eventi a cui sei registrato</div>
+        <div class="events-list">
+            <!-- TODO foreach -->
+            <?php
+            include resource_path('views\components\event-square.blade.php');
+            ?>
+        </div>
+        <div class="section-title">Eventi che hai visualizzato</div>
+        <div class="events-list">
+            <!-- TODO foreach -->
+            <?php
+            include resource_path('views\components\event-square.blade.php');
+            ?>
+        </div>
+    </div>
+
+    <div class="right-side-column">
+        <div class="section-title">Filtro distanza/data</div>
+        <div class="events-parameters-selection-box"></div>
+        <div class="section-title">Filtra per interesse</div>
+        <div class="categories-selection-box">
+        </div>
+    </div>
+@endsection
+<!--<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    </h2>
+</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                You're logged in!
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+</x-app-layout>-->
