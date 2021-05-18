28972e277a34795b02abc08d19023d232633accd
Piereligio <piereligio@gmail.com>
2021-05-18 19:02:19 +0200

    Base HTML Area Personale

---
+++ resources/views/components/event-square.blade.php
@@ -0,0 +1,14 @@
+<a class="event-square" href="/event">
+    <div class="event-square-image-container">
+        <!--TODO inserire immagine <img src="imgBOH" alt="BOHBOH">-->
+    </div>
+    <div class="event-square-title">Event title</div>
+    <div class="event-square-attributes-group">
+        <div class="event-square-attribute">
+            Place
+        </div>
+        <div class="event-square-attribute">
+            Date
+        </div>
+    </div>
+</a>
