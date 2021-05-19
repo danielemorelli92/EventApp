e4269292538fb966dc937dcef102d23b409e11fe
Piereligio <piereligio@gmail.com>
2021-05-19 13:58:11 +0200

    backup dashboard laravel; fix area personale (che sta in dashboard.blade.php)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,27 +23,10 @@
     </div>
 
     <div class="right-side-column">
-        <div class="section-title">Filtro distanza/data</div>
+        <div class="section-title">I tuoi gusti</div>
         <div class="events-parameters-selection-box"></div>
-        <div class="section-title">Filtra per interesse</div>
-        <div class="categories-selection-box">
-        </div>
+        <div class="section-title">Calendario</div>
+        <div class="categories-selection-box"></div>
+        <button style="margin-top: auto">Modifica account</button>
     </div>
 @endsection
-<!--<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-    </h2>
-</x-slot>
-
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                You're logged in!
-            </div>
-        </div>
-    </div>
-</div>
-</x-app-layout>-->
