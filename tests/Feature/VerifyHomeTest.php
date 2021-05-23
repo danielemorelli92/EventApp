b77ef473062af2738bd5920bb24cd9461201c7c2
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-05-23 23:10:21 +0200

    EA-84 test_a_user_can_visualize_12_events_sorted_by_data_and_distance adesso funziona come dovrebbe.

--- tests/Feature/VerifyHomeTest.php
+++ tests/Feature/VerifyHomeTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use App\Models\Event;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
+use function PHPUnit\Framework\isNull;
 
 
 class VerifyHomeTest extends TestCase
@@ -15,14 +17,16 @@ class VerifyHomeTest extends TestCase
 
     use RefreshDatabase;
 
-    public function test_in_evidenza_event_page_can_be_rendered(){
+    public function test_in_evidenza_event_page_can_be_rendered()
+    {
 
         $response = $this->get('/events-highlighted?'); // richiesta get da parte di guest
 
         $response->assertStatus(200);
     }
-    public function test_a_user_can_visualize_12_events_sorted_by_data_and_distance(){
 
+    public function test_a_user_can_visualize_12_events_sorted_by_data_and_distance()
+    {
         Event::factory(150)->create();
 
         $html_content = $this->get('/events-highlighted')->content();
@@ -34,27 +38,19 @@ class VerifyHomeTest extends TestCase
         // da qui in poi $actual conterrà la lista degli id degli eventi
         // che compaiono nella pagina (in base all'ordine in cui compaiono)
 
-        $expected = Event::query()
-            ->where('starting_time', '>=', date(now()))
-            ->orderBy('starting_time')
-            ->get();
+        $expected = Event::query()   // cerca gli eventi
+        ->where('starting_time', '>=', date(now()))  // tra quelli che devono ancora iniziare
+        ->orderBy('starting_time')      // ordinati in base alla data di inizio
+        ->get();
 
-        $count = 0;
-        foreach ($expected as $event) {
-            if ($count == 12 || $event->getDistanceToMe() > 25) {
-                unset($event, $expected);
-            } else {
-                $count++;
-            }
-        }
-        $expected = array_map(function ($event) {
-            return $event->id;
-        }, $expected->toArray());
+        $expected = $expected->filter(function (Event $event) {  //filtra, per ogni evento...
+            return $event->getDistanceToMe() <= 25;   // a una distanza non superiore di 25km
+        })->values()    // solo i valori
+        ->pluck('id') // select sull'ID
+        ->splice(12); // solo i primi 12
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected->toArray(), $actual);
     }
-
-
 }
 
 
