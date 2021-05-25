fedb2029fb26a7ee4ee5edc12aee80d0bad6806a
Piereligio <piereligio@gmail.com>
2021-05-25 17:13:21 +0200

    Modifiche ad area personale

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,20 +6,27 @@
 
 @section('content')
     <div class="main-content-column">
-        <div class="section-title">Eventi a cui sei registrato</div>
+        <div class="section-title">Eventi a cui parteciperai</div>
         <div class="events-list">
             <!-- TODO foreach -->
             <?php
             include resource_path('views\components\event-square.blade.php');
             ?>
         </div>
-        <div class="section-title">Eventi che hai visualizzato</div>
+        <div class="section-title">Eventi a cui hai partecipato</div>
         <div class="events-list">
             <!-- TODO foreach -->
             <?php
             include resource_path('views\components\event-square.blade.php');
             ?>
         </div>
+    </div>
+    <div class="section-title">Eventi suggeriti in base ai tuoi gusti</div>
+    <div class="events-list">
+        <!-- TODO foreach -->
+        <?php
+        include resource_path('views\components\event-square.blade.php');
+        ?>
     </div>
 
     <div class="right-side-column">
