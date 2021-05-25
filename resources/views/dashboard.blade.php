a4f38c78594dbe1928b3d87d9fd1df7ac145e109
Piereligio <piereligio@gmail.com>
2021-05-25 17:14:59 +0200

    Modifiche ad area personale

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,17 +20,17 @@
             include resource_path('views\components\event-square.blade.php');
             ?>
         </div>
-    </div>
-    <div class="section-title">Eventi suggeriti in base ai tuoi gusti</div>
-    <div class="events-list">
-        <!-- TODO foreach -->
-        <?php
-        include resource_path('views\components\event-square.blade.php');
-        ?>
+        <div class="section-title">Eventi suggeriti in base ai tuoi gusti</div>
+        <div class="events-list">
+            <!-- TODO foreach -->
+            <?php
+            include resource_path('views\components\event-square.blade.php');
+            ?>
+        </div>
     </div>
 
     <div class="right-side-column">
-        <button style="margin-top: auto">Modifica account</button>
+        <button style="">Modifica account</button>
         <div class="section-title">I tuoi gusti</div>
         <div class="events-parameters-selection-box"></div>
     </div>
