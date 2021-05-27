b44193082bbfae41822fcaf92a1c1229c8054271
Piereligio <piereligio@gmail.com>
2021-05-27 13:52:51 +0200

    Prima implementazione ricerca per categorie

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{Event,Tag};
+use App\Models\{Event, Tag};
 
 use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
@@ -13,22 +14,31 @@ class EventController extends Controller
     public function index()
     {
         $param = request()->request->all();
-        foreach($param as $key => $value) {
+        foreach ($param as $key => $value) {
             if (blank($value)) {
                 unset($param[$key]);
             }
         }
 
         $query = Event::query();
+        $category_events = collect();
 
         if (count($param) > 0) {
 
             if (array_key_exists('search', $param) and !blank($param['search'])) {
-                $query->where('title','like','%' . $param['search'] . '%');
-                $query->orWhere('description','like','%' . $param['search'] . '%');
+                $query->where('title', 'like', '%' . $param['search'] . '%');
+                $query->orWhere('description', 'like', '%' . $param['search'] . '%');
             }
             if (array_key_exists('luogo', $param) and !blank($param['luogo'])) {
                 //$query->where('address', 'like', $param['luogo']);
+            }
+            if (array_key_exists('categories', $param)) {
+                foreach ($param['categories'] as $category) {
+                    $this_tag = Tag::query()->where('id', '=', $category)->get()[0];
+                    foreach ($this_tag->events as $item) {
+                        $category_events->push($item);
+                    }
+                }
             }
             /*if (!blank($param['dist-max'])) {
                 $filters[] = ['']
@@ -56,21 +66,40 @@ class EventController extends Controller
                     default:
                         break;
                 }
-            }
-            if(array_key_exists('categories', $param)) {
-                $filters[] = ['t']
             }*/
         }
 
         return view('events', [
-            'events' => $query->get(),
+            'events' => $category_events,
             'tags' => Tag::all()
         ]);
     }
-    public function show(Event $event) {
+
+    public function show(Event $event)
+    {
         return view('event', [
             'event' => $event
         ]);
+    }
+
+    public function dashboard()
+    {
+        $query = Event::query()->where('starting_time', '>=', date(now()));
+        $events = $query->get();
+        $registered_events = [];
+
+        if (Auth::check()) {
+            foreach ($events as $event) {
+                if ($event->users->contains(Auth::user())) {
+                    $registered_events[] = $event;
+                }
+            }
+        }
+
+        return view('dashboard', [
+            'registered_events' => $registered_events
+        ]);
+
     }
 
     protected function getDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo)
