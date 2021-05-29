a834a6562a58fa56083cb28b28b55384ca7f8c32
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-05-29 21:06:03 +0200

    EA-78 Creati gli stub della suite EventManagementTest

--- tests/Feature/EventManagementTest.php
+++ tests/Feature/EventManagementTest.php
@@ -8,8 +8,33 @@ use Tests\TestCase;
 
 class EventManagementTest extends TestCase
 {
-    public function test_a_organizator()
+    public function test_a_organizer_can_see_the_create_event_form()
     {
+        $this->assertTrue(false);
+    }
 
+    public function test_a_normal_user_cannot_see_the_create_event_form()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_user_can_delete_a_own_event()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_user_cannot_delete_a_event_of_someone_else()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_user_can_modify_a_own_event()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_user_cannot_modify_a_event_of_someone_else()
+    {
+        $this->assertTrue(false);
     }
 }
