661cb41feaaaedceae77471bf38862d89cdbd53f
solazzol <solazzolorenzo@libero.it>
2021-05-29 20:03:11 +0200

    EA-95 - working in progress

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -13,7 +13,14 @@ class RequestController extends Controller
 
     public function save()
     {
-
+        \App\Models\Request::create([
+            'user_id' => Auth::user()->id,
+            'nome'=> request('nome'),
+            'cognome'=>request('cognome'),
+            'data_nascita'=>request('data_nascita'),
+            'codice_documento'=>request('codice_documento'),
+            'tipo_documento'=>request('tipo_documento')
+        ]);
     }
 
 }
