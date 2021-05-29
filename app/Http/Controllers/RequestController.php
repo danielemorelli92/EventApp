abad022d3529122d7602568281c53f0ea8f3a8a1
solazzol <solazzolorenzo@libero.it>
2021-05-29 19:41:07 +0200

    EA-95 - working in progress

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    public function create()
+    {
+        return view('request');
+    }
+
+    public function save()
+    {
+
+    }
+
+}
