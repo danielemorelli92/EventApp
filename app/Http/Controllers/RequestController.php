805fe1f59a1c5145c37e2e75a4d009f53b41c281
Piereligio <piereligio@gmail.com>
2021-06-03 12:34:45 +0200

    Fix a RequestController.php per cui non salva una eventuale nuova richiesta abilitazione

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,7 +11,7 @@ class RequestController extends Controller
 {
     public function create()
     {
-        if (! Gate::allows('create-request')){
+        if (!Gate::allows('create-request')) {
             abort(401);
         }
 
@@ -24,18 +23,21 @@ class RequestController extends Controller
         if (Gate::denies('create-request')) {
             abort(401);
         }
+        if (\App\Models\Request::query()->where('user_id', "=", Auth::id())->get()->first() == null) {
+            $validatedData = request()->validate([
+                'nome' => 'required|string|max:255',
+                'cognome' => 'required|string|max:255',
+                'data_nascita' => 'required|date',
+                'codice_documento' => 'required|string',
+                'tipo_documento' => 'required|string'
+            ]);
 
-        $validatedData = request()->validate([
-            'nome' => 'required|string|max:255',
-            'cognome' => 'required|string|max:255',
-            'data_nascita' => 'required|date',
-            'codice_documento' => 'required|string',
-            'tipo_documento' => 'required|string'
-        ]);
+            $validatedData['user_id'] = Auth::id();
 
-        $validatedData['user_id'] = Auth::id();
 
-        \App\Models\Request::create($validatedData);
+            \App\Models\Request::create($validatedData);
+        }
+
 
         return redirect('/dashboard');
     }
