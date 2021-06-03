2a7fe60f7d4e06c24ea99106d75b98a78a08e39f
solazzol <solazzolorenzo@libero.it>
2021-06-03 15:43:10 +0200

    EA-10 completato

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static make(array $array)
+ */
 class Request extends Model
 {
     use HasFactory;
