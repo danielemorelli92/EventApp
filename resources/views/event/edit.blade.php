142b59b4a6cf5ce819c14c6c8b752ec12e578489
Piereligio <piereligio@gmail.com>
2021-06-04 20:31:38 +0200

    Inserito check sui campi di registrazione esterna, sulla base del radio selezionato

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -18,10 +18,25 @@
                     <div class="big-form-row"><label class="big-form-label" for="type">Tipo evento</label><input class="big-form-compact-field" type="text" name="type" value="{{ $event->type }}" required></div>
                     <div class="big-form-row"><label class="big-form-label" for="max_partecipants">Numero partecipanti</label><input class="big-form-compact-field" type="number" min="0" name="max_partecipants" value="{{ $event->max_partecipants }}" placeholder=""></div>
                     <div class="big-form-row"><label class="big-form-label" for="price">Prezzo</label><input class="big-form-compact-field" type="number" min="0" step="0.01" name="price" value="{{ $event->price }}" placeholder=""></div>
-                    <div class="big-form-row"><label class="big-form-label" for="ticket_office">Biglietteria</label><input class="big-form-compact-field" type="text" name="ticket_office" value="{{ $event->ticket_office }}"></div>
-                    <div class="big-form-row"><label class="big-form-label" for="website">Sito web</label><input class="big-form-compact-field" type="text" name="website" value="{{ $event->website }}"></div>
+                    <div class="big-form-row"><label class="big-form-label" for="ticket_office">Biglietteria</label><input class="big-form-compact-field" type="text" id="ticket_office" name="ticket_office" value="{{ $event->ticket_office }}"></div>
+                    <div class="big-form-row"><label class="big-form-label" for="website">Sito web</label><input class="big-form-compact-field" type="text" id="website" name="website" value="{{ $event->website }}"></div>
                     <div class="big-form-row"><label class="big-form-label" for="starting_time">Data inizio evento</label><input class="big-form-compact-field" type="datetime-local" name="starting_time" value="{{ date('Y-m-d\TH:i:s', strtotime($event->starting_time)) }}" required></div>
                     <div class="big-form-row"><label class="big-form-label" for="ending_time">Data fine evento</label><input class="big-form-compact-field" type="datetime-local" name="ending_time" value="{{ $event->ending_time != null ? date('Y-m-d\TH:i:s', strtotime($event->ending_time)) : null }}"></div>
+                    <div class="big-form-row" style="width: auto">
+                        <label class="big-form-label" style="min-width: 400px" for="external_registration">Richiedi registrazione esterna</label>
+                        <div class="radio-selection-item" style="flex-grow: 1">
+                            <input onclick="document.getElementById('ticket_office').required = false; document.getElementById('website').required = false" type="radio" class="radio-selection-item-radio" name="external_registration" value="none" checked>
+                            <label for="external_registration" class="radio-selection-item-label" >No</label>
+                        </div>
+                        <div class="radio-selection-item" style="flex-grow: 1">
+                            <input onclick="document.getElementById('ticket_office').required = true; document.getElementById('website').required = false" type="radio" class="radio-selection-item-radio" name="external_registration" value="none">
+                            <label for="external_registration" class="radio-selection-item-label" >Dalla biglietteria</label>
+                        </div>
+                        <div class="radio-selection-item" style="flex-grow: 1">
+                            <input onclick="document.getElementById('website').required = true; document.getElementById('ticket_office').required = false" type="radio" class="radio-selection-item-radio" name="external_registration" value="none">
+                            <label for="external_registration" class="radio-selection-item-label" >Dal sito web</label>
+                        </div>
+                    </div>
 
                     <button class="big-form-submit-button" type="submit" value="Applica modifiche">Applica modifiche</button>
                 </div>
