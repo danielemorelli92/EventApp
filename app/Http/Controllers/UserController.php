d263cf475b64fe7e557a473b39623d21ac937409
solazzol <solazzolorenzo@libero.it>
2021-06-07 22:28:33 +0200

    EA-96 implementazione pagina html della pagina utente (inizio)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -14,5 +16,14 @@ class UserController extends Controller
         return view('user-profile', [
             'user' => $user
         ]);
+    }
+
+    public function user_profile(User $user)
+    {
+        $registered_events_past = Event::all()->where('starting_time' <= date(now()));
+            return view('user-profile', [
+                'registered_events_past' => $registered_events_past,
+                'user' => $user
+            ]);
     }
 }
