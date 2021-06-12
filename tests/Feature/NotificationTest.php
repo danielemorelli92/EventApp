8f9ae5110ecfdbb715ec82daff83cbfa30831372
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-06-12 21:11:25 +0200

    EA-22 Implementati tutti i test per le notifiche

--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -4,37 +4,116 @@ namespace Tests\Feature;
 
 use App\Models\Event;
 use App\Models\User;
+use App\Notifications\AddressChanged;
+use App\Notifications\DateChanged;
+use App\Notifications\DescriptionChanged;
+use App\Notifications\EventCanceled;
+use App\Notifications\TitleChanged;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Notifications\Channels\DatabaseChannel;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class NotificationTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase;
 
-    protected $event;
-    protected $user;
-    protected $admin;
 
     public function test_a_user_registered_for_an_event_receives_a_notification_if_the_title_is_changed()
     {
-        $this->actingAs($this->admin)->put('/events/' . $this->event->id, [
+        $event = Event::factory()->create();
+        $user = User::factory()->create();
+        $event->registeredUsers()->attach($user);
+        $admin = User::factory()->create();
+        DB::table('users')->where('id', $admin->id)->update(['type' => 'admin']);
+
+        $this->actingAs($admin)->put('/events/' . $event->id, [
             'title' => 'new title'
         ]);
 
-        $notifica = $this->user->notifications->first();
+        $notifica = $user->notifications->first();
 
-        self::assertNotNull($notifica, 'non è stata inviata alcuna notifica all\'utente');
-        self::assertEquals('App\Notifications\TitleChanged', $notifica->type, 'la notifica viene inviata ma non è del tipo giusto');
-        self::assertCount(1, $this->user->notifications, 'vengono inviate notifiche non previste');
+        self::assertNotNull($notifica, 'l\'utente non riceve la notifica');
+        self::assertEquals(TitleChanged::class, $notifica->type, 'l\'utente non riceve la giusta notifica');
+        self::assertCount(1, $user->notifications, 'l\'utente riceve più notifiche di quelle attese');
     }
 
-    protected function setUp(): void
+    public function test_a_user_registered_for_an_event_receives_a_notification_if_the_description_is_changed()
     {
-        $this->event = Event::factory()->create();
-        $this->user = User::factory()->create();
-        $this->event->registeredUsers()->attach($this->user);
-        $this->admin = User::factory()->create();
-        $this->admin->update(['type' => 'admin']);
+        $event = Event::factory()->create();
+        $user = User::factory()->create();
+        $event->registeredUsers()->attach($user);
+        $admin = User::factory()->create();
+        DB::table('users')->where('id', $admin->id)->update(['type' => 'admin']);
+
+        $this->actingAs($admin)->put('/events/' . $event->id, [
+            'description' => 'new description'
+        ]);
+
+        $notifica = $user->notifications->first();
+
+        self::assertNotNull($notifica, 'l\'utente non riceve la notifica');
+        self::assertEquals(DescriptionChanged::class, $notifica->type, 'l\'utente non riceve la giusta notifica');
+        self::assertCount(1, $user->notifications, 'l\'utente riceve più notifiche di quelle attese');
+    }
+
+    public function test_a_user_registered_for_an_event_receives_a_notification_if_the_date_is_changed()
+    {
+        $event = Event::factory()->create();
+        $user = User::factory()->create();
+        $event->registeredUsers()->attach($user);
+        $admin = User::factory()->create();
+        DB::table('users')->where('id', $admin->id)->update(['type' => 'admin']);
+
+        $this->actingAs($admin)->put('/events/' . $event->id, [
+            'starting_time' => date(now()->addDays(3)->setHour(3))
+        ]);
+
+        $notifica = $user->notifications->first();
+
+        self::assertNotNull($notifica, 'l\'utente non riceve la notifica');
+        self::assertEquals(DateChanged::class, $notifica->type, 'l\'utente non riceve la giusta notifica');
+        self::assertCount(1, $user->notifications, 'l\'utente riceve più notifiche di quelle attese');
+    }
+
+    public function test_a_user_registered_for_an_event_receives_a_notification_if_the_address_is_changed()
+    {
+        $event = Event::factory()->create();
+        $user = User::factory()->create();
+        $event->registeredUsers()->attach($user);
+        $admin = User::factory()->create();
+        DB::table('users')->where('id', $admin->id)->update(['type' => 'admin']);
+
+        $this->actingAs($admin)->put('/events/' . $event->id, [
+            'address' => 'new address'
+        ]);
+
+        $notifica = $user->notifications->first();
+
+        self::assertNotNull($notifica, 'l\'utente non riceve la notifica');
+        self::assertEquals(AddressChanged::class, $notifica->type, 'l\'utente non riceve la giusta notifica');
+        self::assertCount(1, $user->notifications, 'l\'utente riceve più notifiche di quelle attese');
+    }
+
+    public function test_a_user_registered_for_an_event_receives_a_notification_if_the_event_is_deleted()
+    {
+        $event = Event::factory()->create([
+            'starting_time' => date(now()->addDays(4))
+        ]);
+        $user = User::factory()->create();
+        $event->registeredUsers()->attach($user);
+        $admin = User::factory()->create();
+        DB::table('users')->where('id', $admin->id)->update(['type' => 'admin']);
+
+        $this->actingAs($admin)->delete('/events/' . $event->id);
+
+        $notifica = $user->notifications->first();
+
+        self::assertNotNull($notifica, 'l\'utente non riceve la notifica');
+        self::assertEquals(EventCanceled::class, $notifica->type, 'l\'utente non riceve la giusta notifica');
+        self::assertCount(1, $user->notifications, 'l\'utente riceve più notifiche di quelle attese');
     }
 }
