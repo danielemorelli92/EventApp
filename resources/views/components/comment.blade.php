229dae7b05938c3c15843abe67a9e9534421959f
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-06-14 19:40:07 +0200

    EA-178 Gli utenti non loggati non dovrebbero poter inviare commenti o rispondere

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -8,19 +8,21 @@
                 @endif
             </strong> scrive:</p>
         <textarea name="" id="" cols="100" readonly>{{ $comment->content }}</textarea>
-        <div style="color: blue; "
-             onclick="
-                 document.getElementById('response_{{$comment->id}}').hidden = false;
-                 document.getElementById('content-area_{{$comment->id}}').focus();"
-        >Rispondi
-        </div>
-        <form id="response_{{$comment->id}}" action="/comment/{{$event->id}}/{{$comment->id}}" method="POST" hidden
-              onfocusout="
-                            document.getElementById('response_{{$comment->id}}').hidden = true;
-                            ">
-            <textarea cols="100" rows="3" name="content" id="content-area_{{$comment->id}}"></textarea>
-            <input id="rispondi" type="submit" value="INVIA">
-        </form>
+        @if(\Illuminate\Support\Facades\Auth::check())
+            <div style="color: blue; "
+                 onclick="
+                     document.getElementById('response_{{$comment->id}}').hidden = false;
+                     document.getElementById('content-area_{{$comment->id}}').focus();"
+            >Rispondi
+            </div>
+            <form id="response_{{$comment->id}}" action="/comment/{{$event->id}}/{{$comment->id}}" method="POST" hidden
+                  onfocusout="
+                                document.getElementById('response_{{$comment->id}}').hidden = true;
+                                ">
+                <textarea cols="100" rows="3" name="content" id="content-area_{{$comment->id}}"></textarea>
+                <input id="rispondi" type="submit" value="INVIA">
+            </form>
+        @endif
     </div>
     @foreach($comment->comments as $comment)
         <ul style="list-style-type: none;">
