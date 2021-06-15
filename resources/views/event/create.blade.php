6e819707951806dcf07ef9b54fd308af4216a1d5
solazzol <solazzolorenzo@libero.it>
2021-06-15 19:57:48 +0200

    EA-154 piccoli fix

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -11,16 +11,16 @@
                 @csrf
                 <h2 style="margin-bottom: 20px;">Informazioni dell'evento</h2>
                 <div class="big-form-group">
-                    <div class="big-form-column"><label class="big-form-label" for="title">Titolo</label>
+                    <div class="big-form-column"><label class="big-form-label" for="title">Titolo*</label>
                         <input class="big-form-big-field" name="title" type="text" placeholder="..." required></div>
-                    <div class="big-form-column"><label class="big-form-label" for="description">Descrizione</label><input class="big-form-big-field" type="text" name="description" placeholder="..." required></div>
-                    <div class="big-form-column"><label class="big-form-label" for="address">Indirizzo</label><input class="big-form-big-field" type="text" name="address" required></div>
-                    <div class="big-form-row"><label class="big-form-label" for="type">Tipo evento</label><input class="big-form-compact-field" type="text" name="type" required></div>
+                    <div class="big-form-column"><label class="big-form-label" for="description">Descrizione*</label><input class="big-form-big-field" type="text" name="description" placeholder="..." required></div>
+                    <div class="big-form-column"><label class="big-form-label" for="address">Indirizzo*</label><input class="big-form-big-field" type="text" name="address" required></div>
+                    <div class="big-form-row"><label class="big-form-label" for="type">Tipo evento*</label><input class="big-form-compact-field" type="text" name="type" required></div>
                     <div class="big-form-row"><label class="big-form-label" for="max_partecipants">Numero partecipanti</label><input class="big-form-compact-field" type="number" min="0" name="max_partecipants" placeholder=""></div>
                     <div class="big-form-row"><label class="big-form-label" for="price">Prezzo</label><input class="big-form-compact-field" type="number" min="0" step="0.01" name="price" placeholder=""></div>
                     <div class="big-form-row"><label class="big-form-label" for="ticket_office">Biglietteria</label><input class="big-form-compact-field" type="text" id="ticket_office" name="ticket_office"></div>
                     <div class="big-form-row"><label class="big-form-label" for="website">Sito web</label><input class="big-form-compact-field" type="text" id="website" name="website"></div>
-                    <div class="big-form-row"><label class="big-form-label" for="starting_time">Data inizio evento</label><input class="big-form-compact-field" type="datetime-local" name="starting_time" required></div>
+                    <div class="big-form-row"><label class="big-form-label" for="starting_time">Data inizio evento*</label><input class="big-form-compact-field" type="datetime-local" name="starting_time" required></div>
                     <div class="big-form-row"><label class="big-form-label" for="ending_time">Data fine evento</label><input class="big-form-compact-field" type="datetime-local" name="ending_time"></div>
                     <div class="big-form-row" style="width: auto">
                         <label class="big-form-label" style="min-width: 400px" for="registration_link">Richiedi registrazione da sito esterno</label>
