cf6718a93b18a39bac9075eb2925ed4c9372d862
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-06-16 18:41:04 +0200

    EA-179 WIP bugfix

--- database/migrations/2021_06_15_151900_create_messages_table.php
+++ database/migrations/2021_06_15_151900_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatsTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
