026605fd579a56d743ff764fc5855926ee0c612f
Piereligio <piereligio@gmail.com>
2021-06-17 20:48:42 +0200

    Fatto stile modifica utente

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,26 +1,51 @@
-<!doctype html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Modifica account</title>
-</head>
-<body>
-<h1>Modifica dati utente</h1>
-<form action="/" style="display: flex; flex-direction: column;">
-    <label for="name">Nome</label>
-    <input id="name" name="name" type="text" value="{{ \Illuminate\Support\Facades\Auth::user()->name }}">
-    <label for="email">Email</label>
-    <input id="email" name="email" type="email" value="{{ \Illuminate\Support\Facades\Auth::user()->email }}">
-    <label for="birthday">Data di nascita</label>
-    <input type="date" id="birthday" name="birthday">
-    <label for="numero_telefono">Numero di telefono</label>
-    <input id="numero_telefono" name="numero_telefono" type="tel"
-           value="{{ \Illuminate\Support\Facades\Auth::user()->numero_telefono }}">
-    <label for="sito_web">Sitoweb</label>
-    <input type="text" value="{{ \Illuminate\Support\Facades\Auth::user()->sito_web }}" id="sito_web" name="sito_web">
-</form>
-</body>
-</html>
+@extends('layouts.layout-header-one-columns')
+
+
+@section('content')
+    <div class="main-content-column">
+        <div class="big-form-container">
+
+            <form style="display: flex; flex-direction: column;" action="/">
+                @csrf
+
+                <h2 style="margin-bottom: 20px;">Modifica account</h2>
+
+                <div class="big-form-group">
+
+                    <div class="big-form-row">
+                        <label class="big-form-label" for="name" >Nome</label>
+                        <input class="big-form-compact-field" id="name" name="name" type="text" placeholder="" value="{{ \Illuminate\Support\Facades\Auth::user()->name }}">
+                    </div>
+
+
+                    <div class="big-form-row">
+                        <label class="big-form-label" for="email" >Cognome</label>
+                        <input id="email" class="big-form-compact-field" name="email" type="email" value="{{ \Illuminate\Support\Facades\Auth::user()->email }}">
+                    </div>
+
+                    <div class="big-form-row">
+                        <label class="big-form-label" >Data di nascita</label>
+                        <input type="date" id="birthday" name="birthday" class="big-form-compact-field" placeholder="">
+                    </div>
+
+                    <div class="big-form-row">
+                        <label class="big-form-label"  for="numero_telefono">Numero di telefono</label>
+                        <input class="big-form-compact-field"  id="numero_telefono" name="numero_telefono" type="tel"
+                               value="{{ \Illuminate\Support\Facades\Auth::user()->numero_telefono }}">
+                    </div>
+
+                    <div class="big-form-row">
+                        <label class="big-form-label" for="name" >Sito web</label>
+                        <input class="big-form-compact-field" id="sito_web" name="sito_web" type="text" placeholder="" value="{{ \Illuminate\Support\Facades\Auth::user()->sito_web }}">
+                    </div>
+
+                    <button class="big-form-submit-button" type="submit">Invia</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+
+@endsection
