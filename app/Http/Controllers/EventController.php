d3da376ddf80764effb435c4c74ab94bae507208
danielemorelli92 <daniele.morelli1@student.univaq.it>
2021-06-22 16:41:00 +0200

    EA-208 Test implementati e piccole correzioni di bug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -110,10 +110,10 @@ class EventController extends Controller
             return $event->getDistanceToMe() > 25 && $event->getDistanceToMe() <= 100 && $event->isNotInPromo(); //tra i 25 e i 100km, in promo
         }))->union($futureEvents->filter(function (Event $event) {
             return $event->getDistanceToMe() > 100; //oltre i 100km
-        }));
+        }))->splice(30);
 
         return view('events-highlighted', [
-            'events' => $futureEvents//$events->flatten()
+            'events' => $events
         ]);
     }
 
